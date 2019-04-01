c38040ffcf335d85438e38a9338a495cfe43e479
Jianyong Tang <32235023+tangjy2015@users.noreply.github.com>
2019-03-31 23:30:34 -0700

    Add files via upload

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -20,18 +19,47 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
         
+        # Try not to use pooling for spatial resolution concern
+        self.conv1 = nn.Conv2d(1, 32, 5, 3, 0)  # output size （W-K+2P）/ S + 1 = 74
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 0)  # output size （W-K+2P）/ S + 1 = 72
+        self.conv3 = nn.Conv2d(64, 128, 5, 3, 1)  # output size （W-K+2P）/ S + 1 = 24
+        self.conv4 = nn.Conv2d(128, 256, 3, 3, 0)  # output size （W-K+2P）/ S + 1 = 8
+        self.conv5 = nn.Conv2d(256, 512, 5, 1, 0)  # output size （W-K+2P）/ S + 1 = 4
+        self.conv5_drop = nn.Dropout(p=0.3)
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.pool = nn.MaxPool2d(2, 2) 
         
-
+        self.fc1 = nn.Linear(4*4*512, 1024)
+        self.fc1_drop = nn.Dropout(p=0.2)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc2_drop = nn.Dropout(p=0.2)
+        self.fc3 = nn.Linear(1024, 136)
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        # Convolution layers
+        # Try not to use pooling for spatial resolution concern
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = self.conv5_drop(x)
+        
+        # Flatten and Dense Layers
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc2_drop(x)
+        x = self.fc3(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
+    
